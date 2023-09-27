b37cbba6dcd4339627c8da091373c0eccf308604
Archit <architojha420@gmail.com>
2023-09-27 08:54:12 +0530

    updated project screen

--- screens/ProjectScreen.js
+++ screens/ProjectScreen.js
@@ -1,267 +1,279 @@
-  import * as React from 'react';
-  import {
-    Animated,
-    Dimensions,
-    Image,
-    FlatList,
-    Text,  
-    View,
-    TouchableOpacity,
-    StyleSheet,
-    Linking,
-    ImageBackground,
-    SafeAreaView,
-  } from 'react-native';
+/**
+ *
+ * Inspiration: https://dribbble.com/shots/3731362-Event-cards-iOS-interaction
+ */
 
-  import data, {detailsList, iconsByType} from '../constants/data';
-  import { SimpleLineIcons } from '@expo/vector-icons';
-  import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
-  import {Easing, Transition, Transitioning, elastic} from 'react-native-reanimated'
-  import { StatusBar } from 'expo-status-bar';
-  import { themeColors } from '../theme';
-  import { useNavigation } from '@react-navigation/native'
-  import { ArrowLeftCircleIcon} from 'react-native-heroicons/outline';
-  import { Directions, FlingGestureHandler,State } from 'react-native-gesture-handler';
-  import posed, {Transition as PoseTransition} from 'react-native-pose';
+import * as React from 'react';
+import {
+  StatusBar,
+  Image,
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
+const { width } = Dimensions.get('screen');
+import { EvilIcons } from '@expo/vector-icons';
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from 'react-native-gesture-handler';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
-  const { width, height } = Dimensions.get('screen');
-  const ios = Platform.OS == 'ios';
+// https://www.creative-flyers.com
+const DATA = [
+  {
+    title: 'Afro vibes',
+    location: 'Mumbai, India',
+    date: 'Nov 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Afro-vibes-flyer-template.jpg',
+  },
+  {
+    title: 'Jungle Party',
+    location: 'Unknown',
+    date: 'Sept 3rd, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2019/11/Jungle-Party-Flyer-Template-1.jpg',
+  },
+  {
+    title: '4th Of July',
+    location: 'New York, USA',
+    date: 'Oct 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/4th-Of-July-Invitation.jpg',
+  },
+  {
+    title: 'Summer festival',
+    location: 'Bucharest, Romania',
+    date: 'Aug 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Summer-Music-Festival-Poster.jpg',
+  },
+  {
+    title: 'BBQ with friends',
+    location: 'Prague, Czech Republic',
+    date: 'Sept 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/BBQ-Flyer-Psd-Template.jpg',
+  },
+  {
+    title: 'Festival music',
+    location: 'Berlin, Germany',
+    date: 'Apr 21th, 2021',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Festival-Music-PSD-Template.jpg',
+  },
+  {
+    title: 'Beach House',
+    location: 'Liboa, Portugal',
+    date: 'Aug 12th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Summer-Beach-House-Flyer.jpg',
+  },
+];
 
-  const DURATION=700;
-  const TITLE_SIZE=36;
-  const SPACING = 80;
-  const IMAGE_SIZE =width*0.8;
+const OVERFLOW_HEIGHT = 70;
+const SPACING = 10;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const VISIBLE_ITEMS = 3;
 
-  const colors={
-    lightBg: '#f2f2f2' ,
-    darkBg: '#2c2d51' ,
-    lightText: '#e5e5dd' ,
-    darkText: '#a5a6aa' ,
-  };
-
-  const Item =({ children, style})=>{
-    return(
-      <View
-        style={[
-          {
-            justifyContent: 'center',
-            overflow: 'hidden',
-            backgroundColor: 'transparent',
-          },
-          style,
-        ]}>
-        {children}
-      </View>
-    );
-  };
-
-  const Icon=({ type })=>{
-    return(
-      <SimpleLineIcons
-        name={type}
-        size={26}
-        color='#A5A6AA'
-        style={{marginRight: 15, height: 26}}
-        >
-      </SimpleLineIcons>
-    );
-  };
-
-  const Description=({ index,text,color})=>{
-    return(
-      <Item>
-        <Text key={`description-${index}`} style={{fontSize: 16,color}}>
-          {text}
-        </Text>
-      </Item>
-    )
-  }
-
-  const Title=({ index,text,color})=>{
-    return(
-      <Item style={{height: TITLE_SIZE*3, justifyContent: 'flex-end'}}>
-        <Text 
-          key={`title-${index}`} 
-          style={{
-            fontSize: TITLE_SIZE,
-            fontWeight: '900',
-            color,
-            }}>
-          {text}
-        </Text>
-      </Item>
-    );
-  };
-  const Details=({color,index})=>{
-    return(
-      <View style={{marginVertical: SPACING}}>
-        {detailsList.map((key)=>{
-          return(
-            <View
-              key={key}
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginBottom: 25,
-              }}>
-            <Icon type={iconsByType[key]}></Icon>
-            <Item style={{ flex:1,height: 26, justifyContent: 'center'}}>
-                <Text
-                  key={`${key}-${index}`}
-                  style={{ fontSize: 16,color,fontWeight: '700'}}
-                >
-                  {data[index][key]}
+const OverflowItems = ({ data, scrollXAnimated }) => {
+  const inputRange = [-1, 0, 1];
+  const translateY = scrollXAnimated.interpolate({
+    inputRange,
+    outputRange: [OVERFLOW_HEIGHT, 0, -OVERFLOW_HEIGHT],
+  });
+  return (
+    <View style={styles.overflowContainer}>
+      <Animated.View style={{ transform: [{ translateY }] }}>
+        {data.map((item, index) => {
+          return (
+            <View key={index} style={styles.itemContainer}>
+              <Text style={[styles.title]} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <View style={styles.itemContainerRow}>
+                <Text style={[styles.location]}>
+                  <EvilIcons
+                    name='location'
+                    size={16}
+                    color='black'
+                    style={{ marginRight: 5 }}
+                  />
+                  {item.location}
                 </Text>
-            </Item>
-
+                <Text style={[styles.date]}>{item.date}</Text>
+              </View>
             </View>
-          ); 
+          );
         })}
-      </View>
-    );
-  };
-
-const tranisiton = (
-  <Transition.Together>
-    <Transition.Out type='slide-bottom' durationMs={DURATION} interpolation='easeIn'/>
-    <Transition.Change/>
-    <Transition.In type='slide-bottom' durationMs={DURATION} interpolation='easeOut'/>
-  </Transition.Together>
-);
-
-const config={
-  tranisiton:{
-    type: 'tween',
-    duration: DURATION,
-    Easing:  Easing.elastic(0.9),
-  }
+      </Animated.View>
+    </View>
+  );
 };
 
-const PosedView = posed.View ({
-  enter: {opacity: 1, rotate: '0deg'},
-  exit: {opacity: 0, rotate: '180deg'},
+export default gestureHandlerRootHOC(function App() {
+  const [data, setData] = React.useState(DATA);
+  const scrollXIndex = React.useRef(new Animated.Value(0)).current;
+  const scrollXAnimated = React.useRef(new Animated.Value(0)).current;
+  const [index, setIndex] = React.useState(0);
+  const setActiveIndex = React.useCallback((activeIndex) => {
+    scrollXIndex.setValue(activeIndex);
+    setIndex(activeIndex);
+  });
+
+  React.useEffect(() => {
+    if (index === data.length - VISIBLE_ITEMS - 1) {
+      // get new data
+      // fetch more data
+      const newData = [...data, ...data];
+      setData(newData);
+    }
+  });
+
+  React.useEffect(() => {
+    Animated.spring(scrollXAnimated, {
+      toValue: scrollXIndex,
+      useNativeDriver: true,
+    }).start();
+  });
+
+  return (
+    <FlingGestureHandler
+      key='left'
+      direction={Directions.LEFT}
+      onHandlerStateChange={(ev) => {
+        if (ev.nativeEvent.state === State.END) {
+          if (index === data.length - 1) {
+            return;
+          }
+          setActiveIndex(index + 1);
+        }
+      }}
+    >
+      <FlingGestureHandler
+        key='right'
+        direction={Directions.RIGHT}
+        onHandlerStateChange={(ev) => {
+          if (ev.nativeEvent.state === State.END) {
+            if (index === 0) {
+              return;
+            }
+            setActiveIndex(index - 1);
+          }
+        }}
+      >
+        <SafeAreaView style={styles.container}>
+          <OverflowItems data={data} scrollXAnimated={scrollXAnimated} />
+          <FlatList
+            data={data}
+            keyExtractor={(_, index) => String(index)}
+            horizontal
+            inverted
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: 'center',
+              padding: SPACING * 2,
+              marginTop: 50,
+            }}
+            scrollEnabled={false}
+            removeClippedSubviews={false}
+            CellRendererComponent={({
+              item,
+              index,
+              children,
+              style,
+              ...props
+            }) => {
+              const newStyle = [style, { zIndex: data.length - index }];
+              return (
+                <View style={newStyle} index={index} {...props}>
+                  {children}
+                </View>
+              );
+            }}
+            renderItem={({ item, index }) => {
+              const inputRange = [index - 1, index, index + 1];
+              const translateX = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [50, 0, -100],
+              });
+              const scale = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [0.8, 1, 1.3],
+              });
+              const opacity = scrollXAnimated.interpolate({
+                inputRange,
+                outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
+              });
+
+              return (
+                <Animated.View
+                  style={{
+                    position: 'absolute',
+                    left: -ITEM_WIDTH / 2,
+                    opacity,
+                    transform: [
+                      {
+                        translateX,
+                      },
+                      { scale },
+                    ],
+                  }}
+                >
+                  <Image
+                    source={{ uri: item.poster }}
+                    style={{
+                      width: ITEM_WIDTH,
+                      height: ITEM_HEIGHT,
+                      borderRadius: 14,
+                    }}
+                  />
+                </Animated.View>
+              );
+            }}
+          />
+        </SafeAreaView>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
+  );
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    letterSpacing: -1,
+  },
+  location: {
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 12,
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    padding: SPACING * 2,
+  },
+  itemContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  overflowContainer: {
+    height: OVERFLOW_HEIGHT,
+    overflow: 'hidden',
+  },
 });
-
-
-  export default gestureHandlerRootHOC(function App() {
-    const [index, setIndex] = React.useState(0);
-    const color = index % 2 === 1 ? colors.lightText : colors.darkText;
-    const headingColor = index % 2 === 1 ? colors.lightText: colors.darkBg;
-    const activeIndex=React.useRef(new Animated.Value(0)).current;
-    const animation=React.useRef(new Animated.Value(0)).current;
-
-    React.useEffect(()=>{
-      Animated.timing(animation, {
-        toValue: activeIndex,
-        duration: DURATION*0.7,
-        useNativeDriver: true,
-      }).start();
-    });
-
-
-    const setActiveIndex =React.useCallback(newIndex =>{
-      activeIndex.setValue(newIndex);
-      ref.current.animateNextTransition();
-      setIndex(newIndex);
-    })
-
-    const translateY = animation.interpolate({
-      inputRange: [-1,0,1],
-      outputRange: [height,0,-height] 
-    })
-
-    const ref = React.useRef();
-
-    return (
-      <FlingGestureHandler key='up' direction={Directions.UP} 
-        onHandlerStateChange={(ev)=>{
-        if(ev.nativeEvent.state === State.END){
-          if ( index === data.length - 1){
-            
-            return;
-          }
-          setActiveIndex(index + 1); 
-          
-        }
-      }}>
-        <FlingGestureHandler key='down' direction={Directions.DOWN} 
-        onHandlerStateChange={(ev)=>{
-        if(ev.nativeEvent.state === State.END){
-          if ( index === 0){
-            
-            return;
-          }
-          setActiveIndex(index - 1);
-          
-        }
-      }}>
-
-      <SafeAreaView style={styles.container}>
-        <Animated.View
-        style={[
-          StyleSheet.absoluteFillObject,
-          {height:  height*data.length, transform: [{translateY}]},
-        ]}>
-          {data.map((_,i)=>{
-            return(
-              <View
-                key={i}
-                style={{
-                  height,
-                  backgroundColor: i%2 === 0 ? colors.lightBg : colors.darkBg,
-                }}>
-
-              </View>
-            );
-          })}
-          
-        </Animated.View>
-        <PoseTransition>
-          {index % 2 === 0 ? <PosedView
-            key='image0'
-            style={{
-              borderColor: index % 2 === 0 ? colors.darkBg :colors.lightBg
-              }}>
-            <Image source={data[index].image} style={{marginLeft: width*0.85,}}></Image>
-          </PosedView> : <PosedView
-            key='image1'
-            style={{
-              borderColor: index % 2 === 0 ? colors.darkBg :colors.lightBg
-              }}>
-            <Image source={data[index].image} style={{marginLeft: width*0.85}}></Image>
-          </PosedView>}
-        </PoseTransition>
-
-        <Transitioning.View
-          ref={ref}
-          transition={tranisiton}
-          style={{
-            padding: 20,
-            flex: 1,
-            justifyContent: 'space-evenly',
-            position: 'absolute'
-          }}>
-
-            <Title color={headingColor} index={index} text={data[index].title}></Title>
-            <Details color={color} index={index}/>
-            <Description
-              index={index}
-              text={data[index].description}
-              color={headingColor}/>
-        </Transitioning.View>
-        
-      </SafeAreaView>
-      </FlingGestureHandler>
-      </FlingGestureHandler>
-    );
-  });
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-      
-    },
-  });
---
+++ screens/template2.js
@@ -0,0 +1,267 @@
+import * as React from 'react';
+import {
+  Animated,
+  Dimensions,
+  Image,
+  FlatList,
+  Text,  
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Linking,
+  ImageBackground,
+  SafeAreaView,
+} from 'react-native';
+
+import data, {detailsList, iconsByType} from '../constants/data';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import {Easing, Transition, Transitioning, elastic} from 'react-native-reanimated'
+import { StatusBar } from 'expo-status-bar';
+import { themeColors } from '../theme';
+import { useNavigation } from '@react-navigation/native'
+import { ArrowLeftCircleIcon} from 'react-native-heroicons/outline';
+import { Directions, FlingGestureHandler,State } from 'react-native-gesture-handler';
+import posed, {Transition as PoseTransition} from 'react-native-pose';
+
+const { width, height } = Dimensions.get('screen');
+const ios = Platform.OS == 'ios';
+
+const DURATION=700;
+const TITLE_SIZE=36;
+const SPACING = 80;
+const IMAGE_SIZE =width*0.8;
+
+const colors={
+  lightBg: '#f2f2f2' ,
+  darkBg: '#2c2d51' ,
+  lightText: '#e5e5dd' ,
+  darkText: '#a5a6aa' ,
+};
+
+const Item =({ children, style})=>{
+  return(
+    <View
+      style={[
+        {
+          justifyContent: 'center',
+          overflow: 'hidden',
+          backgroundColor: 'transparent',
+        },
+        style,
+      ]}>
+      {children}
+    </View>
+  );
+};
+
+const Icon=({ type })=>{
+  return(
+    <SimpleLineIcons
+      name={type}
+      size={26}
+      color='#A5A6AA'
+      style={{marginRight: 15, height: 26}}
+      >
+    </SimpleLineIcons>
+  );
+};
+
+const Description=({ index,text,color})=>{
+  return(
+    <Item>
+      <Text key={`description-${index}`} style={{fontSize: 16,color}}>
+        {text}
+      </Text>
+    </Item>
+  )
+}
+
+const Title=({ index,text,color})=>{
+  return(
+    <Item style={{height: TITLE_SIZE*3, justifyContent: 'flex-end'}}>
+      <Text 
+        key={`title-${index}`} 
+        style={{
+          fontSize: TITLE_SIZE,
+          fontWeight: '900',
+          color,
+          }}>
+        {text}
+      </Text>
+    </Item>
+  );
+};
+const Details=({color,index})=>{
+  return(
+    <View style={{marginVertical: SPACING}}>
+      {detailsList.map((key)=>{
+        return(
+          <View
+            key={key}
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginBottom: 25,
+            }}>
+          <Icon type={iconsByType[key]}></Icon>
+          <Item style={{ flex:1,height: 26, justifyContent: 'center'}}>
+              <Text
+                key={`${key}-${index}`}
+                style={{ fontSize: 16,color,fontWeight: '700'}}
+              >
+                {data[index][key]}
+              </Text>
+          </Item>
+
+          </View>
+        ); 
+      })}
+    </View>
+  );
+};
+
+const tranisiton = (
+<Transition.Together>
+  <Transition.Out type='slide-bottom' durationMs={DURATION} interpolation='easeIn'/>
+  <Transition.Change/>
+  <Transition.In type='slide-bottom' durationMs={DURATION} interpolation='easeOut'/>
+</Transition.Together>
+);
+
+const config={
+tranisiton:{
+  type: 'tween',
+  duration: DURATION,
+  Easing:  Easing.elastic(0.9),
+}
+};
+
+const PosedView = posed.View ({
+enter: {opacity: 1, rotate: '0deg'},
+exit: {opacity: 0, rotate: '180deg'},
+});
+
+
+export default gestureHandlerRootHOC(function App() {
+  const [index, setIndex] = React.useState(0);
+  const color = index % 2 === 1 ? colors.lightText : colors.darkText;
+  const headingColor = index % 2 === 1 ? colors.lightText: colors.darkBg;
+  const activeIndex=React.useRef(new Animated.Value(0)).current;
+  const animation=React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(()=>{
+    Animated.timing(animation, {
+      toValue: activeIndex,
+      duration: DURATION*0.7,
+      useNativeDriver: true,
+    }).start();
+  });
+
+
+  const setActiveIndex =React.useCallback(newIndex =>{
+    activeIndex.setValue(newIndex);
+    ref.current.animateNextTransition();
+    setIndex(newIndex);
+  })
+
+  const translateY = animation.interpolate({
+    inputRange: [-1,0,1],
+    outputRange: [height,0,-height] 
+  })
+
+  const ref = React.useRef();
+
+  return (
+    <FlingGestureHandler key='up' direction={Directions.UP} 
+      onHandlerStateChange={(ev)=>{
+      if(ev.nativeEvent.state === State.END){
+        if ( index === data.length - 1){
+          
+          return;
+        }
+        setActiveIndex(index + 1); 
+        
+      }
+    }}>
+      <FlingGestureHandler key='down' direction={Directions.DOWN} 
+      onHandlerStateChange={(ev)=>{
+      if(ev.nativeEvent.state === State.END){
+        if ( index === 0){
+          
+          return;
+        }
+        setActiveIndex(index - 1);
+        
+      }
+    }}>
+
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        {height:  height*data.length, transform: [{translateY}]},
+      ]}>
+        {data.map((_,i)=>{
+          return(
+            <View
+              key={i}
+              style={{
+                height,
+                backgroundColor: i%2 === 0 ? colors.lightBg : colors.darkBg,
+              }}>
+
+            </View>
+          );
+        })}
+        
+      </Animated.View>
+      <PoseTransition>
+        {index % 2 === 0 ? <PosedView
+          key='image0'
+          style={{
+            borderColor: index % 2 === 0 ? colors.darkBg :colors.lightBg
+            }}>
+          <Image source={data[index].image} style={{marginLeft: width*0.85,}}></Image>
+        </PosedView> : <PosedView
+          key='image1'
+          style={{
+            borderColor: index % 2 === 0 ? colors.darkBg :colors.lightBg
+            }}>
+          <Image source={data[index].image} style={{marginLeft: width*0.85}}></Image>
+        </PosedView>}
+      </PoseTransition>
+
+      <Transitioning.View
+        ref={ref}
+        transition={tranisiton}
+        style={{
+          padding: 20,
+          flex: 1,
+          justifyContent: 'space-evenly',
+          position: 'absolute'
+        }}>
+
+          <Title color={headingColor} index={index} text={data[index].title}></Title>
+          <Details color={color} index={index}/>
+          <Description
+            index={index}
+            text={data[index].description}
+            color={headingColor}/>
+      </Transitioning.View>
+      
+    </SafeAreaView>
+    </FlingGestureHandler>
+    </FlingGestureHandler>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
